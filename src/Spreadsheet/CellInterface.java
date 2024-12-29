e1d0da0d606c2f939c8217c20c7524b471d0b0d4
ETL-H <itiel.alevi.g@gmail.com>
2024-12-29 07:36:08 +0200

    Added in the given files for the assignment

--- src/Spreadsheet/CellInterface.java
+++
@@ -1,9 +0,0 @@
-package Spreadsheet;
-
-public interface CellInterface {
-    String getValue();  // Get the value of the cell
-    void setValue(String value);  // Set the value of the cell
-    boolean isNumber();  // Check if the value is a number
-    boolean isText();  // Check if the value is text
-    boolean isFormula();  // Check if the value is a formula
-}
